a082bae1a8a336d1f75df6216599793e4a5d71b8
Kharukulavat <nipunk0258@gmail.com>
2023-01-04 01:04:10 +0700

    Create 02_Print_Command.py
    Some of 2.1 problems(1-30)

---
+++ 02_Print_Command.py
@@ -0,0 +1,86 @@
+#Some of 2.1 problems(1-30)
+
+print("Merry X'mas") #>>>Merry X'mas
+
+print("I want to ask you \"Why don't you drive to work ?\"") 
+#>>>I want to ask you "Why don't you drive to work ?"
+
+print("\"I don't have a car\"") #>>> "I don't have a car"
+
+print("/\\/\\/\\") #>>> /\/\/\
+
+print(3.50) #>> 3.5
+
+print('%d' %3) #>> 3
+print('%f' %1) #>> 1.000000
+print('%.2f' %1) #>> 1.00
+print('%s' %7.0) #>> 7.0
+print('%d %.3f %s' %(2, 5, 7.23)) #>> 2 5.000 7.23
+print('%.2f %s %d' %(3, 3.0, 3)) #>> 3.00 3.0 3
+
+from math import pi
+print(pi) #>>> 3.141592653589793
+
+a = 2.4
+b = 2.5
+print(a,"+",b,"=","%.4f"%(a+b)) #>>> 2.4 + 2.5 = 4.9000
+
+a = 5
+b = 2
+print("%.2f = %.2f = %.4f"%(a,b,a-b)) #>>> 5.00 = 2.00 = 3.0000
+
+print(''' 
+a
+an
+ant''') 
+
+print('''
+        *
+*       *       *
+        *
+''')
+#or 
+
+print('''\t*
+*\t*\t*
+      \t*''')
+#>>> 
+#        *
+#*       *       *
+#        * 
+
+print('''
+*       +       *
++       *       +
+*       +       *''')
+
+#or 
+
+print('''
+*\t+\t*
++\t*\t+
+*\t+\t*
+      ''')
+#>>>
+# *       +       *
+# +       *       +
+# *       +       *
+
+print('''
+\\               /
+        x
+           
+/               \\''')
+
+#or 
+
+print('''\\\t\t/
+\tx
+
+/\t\t\\''')
+
+#>>>
+# \               /
+#         x
+
+# /               \
